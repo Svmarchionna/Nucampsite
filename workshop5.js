25131b58553bc966a3d7d1edf61d99ac23312663
Svmarchionna <87159757+Svmarchionna@users.noreply.github.com>
2022-08-05 18:34:11 -0400

    Add files via upload

---
+++ workshop5.js
@@ -0,0 +1,27 @@
+class Student {
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
+console.log(testStudent);
+if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
+    console.log('TASK 1: PASS');
+}
+
+class Bootcamp{
+    constructor(name,level,student){
+        this.name = name;
+        this.level = level;
+        this.student = student;
+    }
+}
+
+reactBootcamp = new Bootcamp("React", "Advanced");
+console.log(reactBootcamp);
+if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
+        && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
+    console.log('TASK 2: PASS');
+}
