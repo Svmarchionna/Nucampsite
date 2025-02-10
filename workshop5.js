0f91863e0384cdd32c7047656e843462a5117c9f
Svmarchionna <87159757+Svmarchionna@users.noreply.github.com>
2025-02-10 10:58:54 -0500

    Update workshop5.js

--- workshop5.js
+++ workshop5.js
@@ -7,21 +7,21 @@ class Student {
 
 testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
 console.log(testStudent);
-if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
+if (testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
     console.log('TASK 1: PASS');
 }
 
 class Bootcamp{
-    constructor(name,level,student){
+    constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
-        this.student = student;
+        this.students = students;
     }
 }
 
-reactBootcamp = new Bootcamp("React", "Advanced");
+reactBootcamp = new Bootcamp("React", "Advanced", []);
 console.log(reactBootcamp);
-if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
-        && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
+if (reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced' 
+    && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
     console.log('TASK 2: PASS');
 }
