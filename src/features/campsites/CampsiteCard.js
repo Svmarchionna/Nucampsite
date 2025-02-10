b82ebbd99239381dc20e163e39cd8bd2ee2941bb
Svmarchionna <87159757+Svmarchionna@users.noreply.github.com>
2025-02-10 11:02:29 -0500

    Update CampsiteCard.js

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,18 +1,28 @@
-import {Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const CampsiteCard = ({ campsite}) => {
+const CampsiteCard = ({ campsite }) => {
     const { id, image, name } = campsite;
     return (
-    <Link to={`${id}`}>
-        <Card>
-            <CardImg width='100%' src={image} alt={name} /> 
-            <CardImgOverlay>
-                <CardTitle>{name}</CardTitle>
-            </CardImgOverlay>      
-        </Card>
-     </Link>
+        <Link to={`${id}`} aria-label={`View details of ${name}`}>
+            <Card>
+                <CardImg width='100%' src={image} alt={name} /> 
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>     
+            </Card>
+        </Link>
     );
 };
 
-export default CampsiteCard; 
+CampsiteCard.propTypes = {
+    campsite: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired
+    }).isRequired
+};
+
+export default CampsiteCard;
